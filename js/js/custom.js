79312ad32b5d1626024022d2e638c3ac43cbcdbc
Artem <artemserdyuk321@gmail.com>
2017-08-07 19:58:05 +0300

    first commit

---
+++ js/js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".owl-carousel2").owlCarousel({
+        loop:true,
+
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:true
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+});
